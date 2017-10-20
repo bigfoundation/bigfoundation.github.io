efa32d30cc80523404059f7704cb2c3d9a0d2f17
Roman Travnikov <travnikovrn@gmail.com>
2017-10-20 20:51:17 +0300

    mobile start

--- js/init.js
+++ js/init.js
@@ -1,8 +1,26 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.button-collapse').sideNav();
-    $('.carousel').carousel();
+        $('.button-collapse').sideNav();
+        $('.carousel').carousel();
+        $('.pushpin').each(function() {
+            var $this = $(this);
+            var $target = $('#' + $(this).attr('data-target'));
+            if ($this.hasClass("mobiloff")) {
+                if (document.documentElement.clientWidth > 600) {
+                    $this.pushpin({
+                        top: $target.offset().top,
+                        bottom: $target.offset().top + $target.outerHeight() - $this.height()
+                    });
+                }
+            } else {
+                $this.pushpin({
+                    top: $target.offset().top,
+                    bottom: $target.offset().top + $target.outerHeight() - $this.height()
+                });
+            }
 
-  }); // end of document ready
+        });
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
